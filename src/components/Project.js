d5c88ab996a0cc179736fc386b8cf1b7d024c8c3
Fayssal <belghazi.fayssal.prof@gmail.com>
2023-09-18 16:48:30 +0200

    Mise en place composant Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../Project.scss';
 
 const Project = () => {
+  const [selectedImage, setSelectedImage] = useState("img/DASH.png")
+
+  const handleListClick = (element) => {
+    setSelectedImage(element);
+  }
+//////////////////////////FAIRE UN CAROUSEL
+  const projects = [
+    { name: "Dash (en cours)", images: ["img/DASH.png","img/athletes.png"], description: "Dash est un jeux d'ambiance" },
+    { name: "Literie3000", images: ["img/DASH.png","img/DASH.png"], description: "Commandez votre matelas de reve" }]
+
   return (
-    <div>Project</div>
+    <div className="image-list-container" >
+      <div className="selected-image">
+        {selectedImage && <img src={selectedImage} alt="Image du projet" />}
+      </div>
+      <ul className="word-list">
+        {projects.map((item, index) => (
+          <li key={index} onClick={() => handleListClick(item.image)}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
